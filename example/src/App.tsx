9059344c7df285fec92caaf2288f891803ac0a9d
enestatli <eenestatli@gmail.com>
2024-09-23 14:49:15 +0300

    feat: init project base

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,30 +1,9 @@
-import { useState, useEffect } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'tiktok-opensdk-react-native';
+import TikTokSDK from 'tiktok-opensdk-react-native';
 
-export default function App() {
-  const [result, setResult] = useState<number | undefined>();
-
-  useEffect(() => {
-    multiply(3, 7).then(setResult);
-  }, []);
-
-  return (
-    <View style={styles.container}>
-      <Text>Result: {result}</Text>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  box: {
-    width: 60,
-    height: 60,
-    marginVertical: 20,
-  },
-});
+TikTokSDK.login('your-client-key', 'your-redirect-uri')
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
--- src/index.tsx
+++ src/index.tsx
@@ -16,21 +16,38 @@ const TiktokOpensdkReactNative = NativeModules.TiktokOpensdkReactNative
         },
       }
     );
-interface LoginResult {
+
+export interface LoginResult {
   authCode: string;
   grantedPermissions: string[];
   authError?: string;
   authErrorDescription?: string;
 }
 
-interface ShareResult {
+export interface ShareResult {
   isSuccess: boolean;
   errorCode: number;
   subErrorCode: number;
   errorMsg: string;
 }
 
-const TikTokSDK = {
+export interface TikTokSDKType {
+  login: (clientKey: string, redirectUri: string) => Promise<LoginResult>;
+  handleLoginResult: (
+    intentUri: string,
+    redirectUri: string
+  ) => Promise<LoginResult>;
+  share: (
+    clientKey: string,
+    mediaUrls: string[],
+    isImage: boolean,
+    isGreenScreen: boolean
+  ) => Promise<void>;
+  handleShareResult: (intentUri: string) => Promise<ShareResult>;
+  grantUriPermission: (uri: string) => Promise<void>;
+}
+
+const TikTokSDK: TikTokSDKType = {
   login: (clientKey: string, redirectUri: string): Promise<LoginResult> => {
     return TiktokOpensdkReactNative.login(clientKey, redirectUri);
   },
